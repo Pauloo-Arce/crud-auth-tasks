d2a1e86a65e42235040b88b9d5183d531e671511
Paulo Arce <paulodigital.contact@gmail.com>
2025-03-01 16:47:18 -0300

    inicando proyecto y configurando credenciales

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <>
+      <form>
+        <input type="email" name="email" placeholder="youremail@gmail.com" />
+        <button>Send</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
